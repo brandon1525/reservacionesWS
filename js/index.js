7fb06dd9ffc618c02266403f27b0cce64954a942
chepepe269 <chepe269@hotmail.com>
2016-05-14 12:26:04 -0500

    agrege el marcador de llegada y trace la ruta, tambien los marcadores tienen iconos nuevos que hacen referencia a la salida y llegada.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var map;
 	function initMap() {
 		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: -34.397, lng: 150.644},
+			center: {lat: 21.880628, lng: -102.29534790000002},
 			zoom: 8
 		});
 	}
