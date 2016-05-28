6e8f4b67df1c2a687ad2a80b28cf98344ebb9ce8
Brandon Alan Rodríguez <br4nd0n4l4n@gmail.com>
2016-05-28 05:21:12 -0500

    Nuevas funcionalidades

--- js/index.js
+++ js/index.js
@@ -29,8 +29,31 @@ $(document).ready(function(){
 	var num=0;
 	var IdHotSelc;
 	var IdAct;
-	$("#btn1").click(function(){
+	//Botones para las secciones 
+	$('#si_sec_vuelos').click(function(){
+		$('#sec_vuelos_forms').show();
+		$(this).closest('.tarjeta_inicio').hide();
+		initMap();
+	});
+	$('#no_sec_vuelos').click(function(){
+		//$('#sec_vuelos_forms').hide();
+		$('#sec_vuelos').find('.collapsible-header').click();
+		$('#sec_vuelos_forms').show();
+		initMap();
+		$(this).closest('.tarjeta_inicio').hide();
+	});
 
+	$('#si_sec_hoteles').click(function(){
+		$('#sec_hoteles_forms').show();
+		$(this).closest('.tarjeta_inicio').hide();
+	});
+	$('#no_sec_hoteles').click(function(){
+		$('#sec_hoteles_forms').hide();
+		$(this).closest('.tarjeta_inicio').hide();
+	});
+	
+	
+	$("#btn_buscar_vuelo").click(function(){
 		if($("#text_salida").val()=="" || $("#text_llegada").val()=="" || $("#fechaS").val()=="" || $("#horaS").val()=="" || $("#num").val()==""){
 			console.log("erro aerta");
 			return false;
@@ -56,13 +79,51 @@ $(document).ready(function(){
 						'<td class="tr_id">'+value.id+'</td>'+
 						'<td>'+value.precio+'</td>'+
 						'</tr>');
-				})
-				
+				});
 			}else{
 				console.log("error alerta");
 			}
 		});
 	});
+	$('#next_hoteles').click(function(){
+		$('#sec_vuelos').find('.collapsible-header').click();
+		if(!$('#sec_hoteles').find('.collapsible-header').hasClass('active')){
+			$('#sec_hoteles').find('.collapsible-header').click();
+		}
+		
+	});
+	$("#btn2").click(function(){
+		console.log("Simon");
+		$.ajax({
+			url : 'http://localhost/reservacionesWS/php/gethotelesdestinohabitacion.php',
+			data : {fecha_ll: 2016-05-26, fecha_s: 2016-05-30, destino: $("#text_llegada").val(),habitaciones_requeridas: $("#num").val()},
+			method : 'POST',
+			dataType : 'json',
+			mimeType: 'application/json'
+		}).done(function(json){
+			if(json.result){
+				$("#table_hoteles tbody").html("");
+				$.each(json.data,function(index,value){
+					//console.log(value.nombre);
+					$("#table_hoteles tbody").append(
+						'<tr class="selec_hotel">'+
+						'<td>'+value.nombre+'</td>'+
+						'<td>'+value.ciudad+'</td>'+
+						'<td>'+value.estado+'</td>'+
+						'<td>'+value.descripcion+'</td>'+
+						'<td>'+value.starts+'</td>'+
+						'<td>'+value.vista_mar+'</td>'+
+						'<td>'+value.buffet+'</td>'+
+						'<td>'+value.barralibre+'</td>'+
+						'<td>'+value.shownocturno+'</td>'+
+						'</tr>'
+					);
+				})
+			}else{
+				console.log("error alerta");
+			}
+		});
+	});	
 	$(document).on("click","#btn3",function(){
 		console.log("Simon entre al 3");
 		$.ajax({
@@ -75,7 +136,7 @@ $(document).ready(function(){
 			if(json.result){
 				$("#actividades tbody").html("");
 				$.each(json.data,function(index,value){
-				$("#actividades tbody").append(
+					$("#actividades tbody").append(
 						'<tr class="selec_actividad" data-id_actividad="'+value.id+'">'+
 						'<td>'+value.nombre+'</td>'+
 						'<td>'+value.ciudad+'</td>'+
@@ -83,23 +144,21 @@ $(document).ready(function(){
 						'<td>$'+value.precio_nino+'</td>'+
 						'<td>$'+value.precio_adulto+'</td>'+
 						'</tr>'
-						);
-
+					);
 				});
-			}else
-			{
+			}else{
 				console.log("cagaste el palo");
 			}
 		    
 		});
 	});
+
 	$(document).on("click",".selec_actividad",function(){
-				IdAct= $(this).data("id_actividad");
+		IdAct= $(this).data("id_actividad");
 		console.log(IdAct);
 	});
 
 	$(document).on("click","#btn2", function(){
-
 		$.ajax({
 		    url : 'http://localhost/reservacionesWS/php/gethotelesdestinohabitacion.php',
 		    data : {fecha_ll: $("#fechaLlHot").val(), fecha_s: $("#fechaSalHot").val(), destino: $("#text_llegada").val(),habitaciones_requeridas: $("#numH").val()},
@@ -168,12 +227,11 @@ $(document).ready(function(){
 						'<td>'+barralibre+'</td>'+
 						'<td>'+shownocturno+'</td>'+
 						'</tr>'
-						);
-				})
+					);
+				});
 			}else{
 				console.log("error alerta");
 			}
-    
 		});
 	});
 
@@ -193,7 +251,7 @@ $(document).ready(function(){
 		}
 	});
 	$(document).on('click','.seleccionar_vuelo',function(){
-		//console.log($(this).find('.tr_id').text());
+		var nuevo_id_vuelo;
 		$('#contenedor_asientos').html('');
 		var contenido = '<div class="col s12 m6">';
 		for(var i =1; i<=72;i++){
@@ -257,7 +315,7 @@ $(document).ready(function(){
 	    		}
 	    	});
 	    });
-	    $("#formularios").append('<div class="form-group"><button type="submit" id="btn2" class="btn btn-default" >Reservar hotel</button><button type="submit" id="btn3" class="btn btn-default" >continuar sin hotel</button></div>');
+	    
 		$.ajax({
 			url : 'http://localhost/reservacionesWS/php/getvueloasientos.php',
 			data : {id_vuelo: $(this).find('.tr_id').text()},
@@ -273,54 +331,50 @@ $(document).ready(function(){
 		});
 		//console.log($(this).find('.tr_id').text());
 	});
-	$("#btn2").click(function(){
-		console.log("Simon");
-		$.ajax({
-		    url : 'http://localhost/reservacionesWS/php/gethotelesdestinohabitacion.php',
-		    data : {fecha_ll: 2016-05-26, fecha_s: 2016-05-30, destino: $("#text_llegada").val(),habitaciones_requeridas: $("#num").val()},
-		    method : 'POST',
-		    dataType : 'json',
-		    mimeType: 'application/json'
-		}).done(function(json){
-			if(json.result){
-				$("#table_hoteles tbody").html("");
-				$.each(json.data,function(index,value){
-					//console.log(value.nombre);
-					$("#table_hoteles tbody").append(
-						'<tr class="selec_hotel">'+
-						'<td>'+value.nombre+'</td>'+
-						'<td>'+value.ciudad+'</td>'+
-						'<td>'+value.estado+'</td>'+
-						'<td>'+value.descripcion+'</td>'+
-						'<td>'+value.starts+'</td>'+
-						'<td>'+value.vista_mar+'</td>'+
-						'<td>'+value.buffet+'</td>'+
-						'<td>'+value.barralibre+'</td>'+
-						'<td>'+value.shownocturno+'</td>'+
-						'</tr>'
-						);
-				})
-			}else{
-				console.log("error alerta");
-			}
-		});
-	});	
+	
 	$(document).on("click",".selec_hotel",function(){
 		IdHotSelc= $(this).data("id_hotel");
 		console.log(IdHotSelc);
 	});
 	var map;
 	function initMap() {
+		var myLatLng = {lat: 23.4598087, lng: -100.3507389};
 		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: 21.880628, lng: -102.29534790000002},
-			zoom: 8
+			zoom: 6,
+			center: myLatLng,
+			scrollwheel: false,
+			navigationControl: false,
+			mapTypeControl: false,
+			scaleControl: false,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
 		});
+		markerS = new google.maps.Marker({
+			map: map,
+			draggable: true,
+			animation: google.maps.Animation.DROP,
+			position: {lat: 21.864865607746523, lng: -102.28433265000001},
+			icon: imageMS
+		});
+		markerLl = new google.maps.Marker({
+			map: map,
+			draggable: true,
+			animation: google.maps.Animation.DROP,
+			position:{lat: 21.24719896448074, lng: -105.11636390000001},
+			icon:imageMLl
+		});
+		geocoder = new google.maps.Geocoder;
+		infowindow = new google.maps.InfoWindow;
+		markerLl.addListener('click', toggleBounceLl);
+		markerLl.addListener('dragend', obtenerInfoLl);
+		markerS.addListener('click', toggleBounceS);
+		markerS.addListener('dragend', obtenerInfoS);
 	}
 	$(window).resize(function () {
 		var h = $(window).height(),
 		offsetTop = 30; // Calculate the top offset
 		$('#map').css('height', (h - offsetTop));
 	}).resize();
+
 	$('#btn_mostrar_mapa').click(function(){
 		if($('#map').is(":visible")){
 			$(this).html('<i class="material-icons left">place</i> Mostrar mapa');
