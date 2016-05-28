e5f35b70bf2bc4c5d7bc19a816d56ee48d1b744b
Brandon Alan Rodríguez <br4nd0n4l4n@gmail.com>
2016-05-28 03:54:03 -0500

    Nueva visualizacion y demas funcionalidades

--- js/index.js
+++ js/index.js
@@ -61,39 +61,6 @@ $(document).ready(function(){
 			}
 		});
 	});
-	$(document).on("click","#btn2", function(){
-		console.log("Simon");
-		$.ajax({
-		    url : 'http://localhost/reservacionesWS/php/gethotelesdestinohabitacion.php',
-		    data : {fecha_ll: 2016-05-26, fecha_s: 2016-05-30, destino: $("#text_llegada").val(),habitaciones_requeridas: $("#num").val()},
-		    method : 'POST',
-		    dataType : 'json',
-		    mimeType: 'application/json'
-		}).done(function(json){
-			if(json.result){
-				$("#table_hoteles tbody").html("");
-				$.each(json.data,function(index,value){
-					console.log(value.nombre);
-					$("#table_hoteles tbody").append(
-						'<tr class="selec_hotel">'+
-						'<td>'+value.nombre+'</td>'+
-						'<td>'+value.ciudad+'</td>'+
-						'<td>'+value.estado+'</td>'+
-						'<td>'+value.descripcion+'</td>'+
-						'<td>'+value.starts+'</td>'+
-						'<td>'+value.vista_mar+'</td>'+
-						'<td>'+value.buffet+'</td>'+
-						'<td>'+value.barralibre+'</td>'+
-						'<td>'+value.shownocturno+'</td>'+
-						'</tr>'
-						);
-				})
-			}else{
-				console.log("error alerta");
-			}
-    
-		});
-	});
 
 	$(document).on("click", ".asiento", function() { 
 		//console.log(asientos);
@@ -111,15 +78,69 @@ $(document).ready(function(){
 	});
 	$(document).on('click','.seleccionar_vuelo',function(){
 		//console.log($(this).find('.tr_id').text());
-		num=0;
-	    $("#fondo-avion").html('<div id="seccion1" class="row"><div class="col-xs-6"><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div></div><div class="col-xs-6"><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div></div></div><div id="seccion2" class="row"><div class="col-xs-6"><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div></div><div class="col-xs-6"><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div></div></div><div id="seccion3" class="row"><div class="col-xs-6"><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div></div><div class="col-xs-6"><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div><div class="col-xs-1 asiento desactivo"></div></div></div>');
-	    $(".asiento").removeClass("activo").addClass("desactivo");
-	    $("#formularios").html("");
+		$('#contenedor_asientos').html('');
+		var contenido = '<div class="col s12 m6">';
+		for(var i =1; i<=72;i++){
+			contenido+='<div class="col s1 asiento_disponible"><i class="tiny material-icons">airline_seat_recline_normal</i><span>'+i+'</span></div>';
+			if(i==12 || i==24 || i==36 || i==48 || i==60 || i==72){
+				contenido+='</div>';
+				if(i!=72){
+					contenido+='<div class="col s12 m6">';
+				}
+				
+				
+			}
+		}
+		$('#contenedor_asientos').append(contenido);
+
+		//$(".asiento").removeClass("activo").addClass("desactivo");
+	    $("#formularios").html('');
+
 	    asientos = $("#num").val();
 	    asientos2 = $("#num").val();
 	    for (var i = 0; i <asientos; i++) {
-	      	$("#formularios").append('<div class="panel panel-default"><div class="panel-body"><form class="form-horizontal" role="form"><div class="form-group"><label class="control-label col-sm-2"for="nombre">Nombre:</label><div class="col-sm-10"><input type="text" class="form-control" id="nombre" placeholder="Nombres"></div>			</div><div class="form-group"><label class="control-label col-sm-2"for="apellidoP">Apellido Paterno:</label><div class="col-sm-10"><input type="text" class="form-control" id="apellidoP" placeholder="Apellido Paterno"></div>			</div><div class="form-group"><label class="control-label col-sm-2"for="apellidoM">Apellido Materno:</label><div class="col-sm-10"><input type="text" class="form-control" id="apellidoM" placeholder="Apellido Materno"></div>		</div><div class="form-group"><label class="control-label col-sm-2 for="nas">Numero de asiento</label><div class="col-sm-10" ><input type="text" class="form-control Nasiento" disabled id="nas"></div></div></form><div class="form-group "><label class="control-label">Sexo: </label><label class="checkbox-inline"><input type="checkbox" id="checkboxEnLinea1" value="Massculino"> Masculino </label><label class="checkbox-inline"><input type="checkbox" id="checkboxEnLinea2" value="Femenino"> Femenino </label></div></form></div></div>');
+	      	$("#formularios").append('<div class="persona_asiento">'+
+									'<div class="row">'+
+										'<h5 class="col s12 text-fluid">Persona '+(i+1)+'</h5>'+
+										'<div class="input-field col s12 m6">'+
+											'<i class="material-icons prefix">perm_identity</i>'+
+											'<input placeholder="Nombres(s)" type="text" class="nombre_pasajero">'+
+											'<label class="active">Nombre(s)</label>'+
+										'</div>'+
+										'<div class="input-field col s6 m3">'+
+											'<i class="material-icons prefix">airline_seat_recline_extra</i>'+
+											'<input placeholder="Número de asiento" type="text" class="asiento_pasajero">'+
+											'<label class="active">Número de asiento</label>'+
+										'</div>'+
+										'<div class="input-field col s6 m3">'+
+											'<input class="with-gap" name="group1" type="radio" data-label_ref="Femenino"/>'+
+											'<label class="active_radiobutton" style="color: #FF6781;">Femenino</label>'+
+											'<input class="with-gap" name="group1" type="radio" data-label_ref="Masculino" />'+
+											'<label class="active_radiobutton" style="color: #039be5;">Masculino</label>'+
+										'</div>'+
+									'</div>'+
+									'<div class="row">'+
+										'<div class="input-field col s12 m6">'+
+											'<i class="material-icons prefix">perm_identity</i>'+
+											'<input placeholder="Apellido paterno" type="text" class="apellidoP_pasajero">'+
+											'<label class="active">Apellido paterno</label>'+
+										'</div>'+
+										'<div class="input-field col s12 m6">'+
+											'<i class="material-icons prefix">perm_identity</i>'+
+											'<input placeholder="Apellido materno" type="text" class="apellidoM_pasajero">'+
+											'<label class="active">Apellido materno</label>'+
+										'</div>'+
+									'</div>'+
+								'</div>');
 	    }
+	    $(document).on('click','.active_radiobutton',function(){
+	    	var element_click=$(this);
+	    	$(this).closest('.input-field').find('input').each(function(index,value){
+	    		if($(value).data('label_ref')==$(element_click).text()){
+	    			$(value).click();
+	    		}
+	    	});
+	    });
 	    $("#formularios").append('<div class="form-group"><button type="submit" id="btn2" class="btn btn-default" >Reservar hotel</button><button type="submit" id="btn3" class="btn btn-default" >continuar sin hotel</button></div>');
 	    $('html,body').animate({scrollTop: $("#fondo-avion").offset().top}, 2000);
 		$.ajax({
@@ -138,7 +159,36 @@ $(document).ready(function(){
 		//console.log($(this).find('.tr_id').text());
 	});
 	$("#btn2").click(function(){
-
+		console.log("Simon");
+		$.ajax({
+		    url : 'http://localhost/reservacionesWS/php/gethotelesdestinohabitacion.php',
+		    data : {fecha_ll: 2016-05-26, fecha_s: 2016-05-30, destino: $("#text_llegada").val(),habitaciones_requeridas: $("#num").val()},
+		    method : 'POST',
+		    dataType : 'json',
+		    mimeType: 'application/json'
+		}).done(function(json){
+			if(json.result){
+				$("#table_hoteles tbody").html("");
+				$.each(json.data,function(index,value){
+					//console.log(value.nombre);
+					$("#table_hoteles tbody").append(
+						'<tr class="selec_hotel">'+
+						'<td>'+value.nombre+'</td>'+
+						'<td>'+value.ciudad+'</td>'+
+						'<td>'+value.estado+'</td>'+
+						'<td>'+value.descripcion+'</td>'+
+						'<td>'+value.starts+'</td>'+
+						'<td>'+value.vista_mar+'</td>'+
+						'<td>'+value.buffet+'</td>'+
+						'<td>'+value.barralibre+'</td>'+
+						'<td>'+value.shownocturno+'</td>'+
+						'</tr>'
+						);
+				})
+			}else{
+				console.log("error alerta");
+			}
+		});
 	});	
 	var map;
 	function initMap() {
